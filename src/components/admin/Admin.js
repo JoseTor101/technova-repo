5c5ce1a291f1e1b48db8941a1e53600dc2cd850f
JoseTor <josetordecilla800@gmail.com>
2021-12-30 18:45:57 -0500

    Instrucciones para la pagina de admin

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -7,7 +7,7 @@ const Admin = () => {
           <Header/>
           <h1>Página del administrador. Bienvenido!</h1>
           <hr></hr>
-          <h2>¡Qué desea hacer hoy?</h2>
+          <h2>¿Qué desea hacer hoy?</h2>
           <ul> 
               <li><button>Consultar</button>
               <ol>
@@ -16,10 +16,29 @@ const Admin = () => {
                   <li>Aplicar filtros de búsqueda</li>
               </ol>
               </li>
-              <li><button>Agregar</button></li>
-              <li><button>Modificar</button></li>
-              <li><button>Eliminar</button></li>
-              
+              <li><button>Agregar</button>
+              <ol>
+                  <li>Ingrese el tipo de producto</li>
+                  <li>De acuerdo al tipo de producto aparece una tabla para llenar on los detalles del tipo de producto seleccionado, por ejemplo si es televisor que aparezca nombre, marca, smartTV, etc.</li>
+                  <li>Todos los campos deben estar diligenciados</li>
+                  <li>Botón para subir el producto</li>
+              </ol>
+              </li>
+              <li><button>Modificar</button>
+              <ol>
+                  <li>Se filtra primero hasta encontrar el producto</li>
+                  <li>Los detalles actuales del producto aparecen en una tabla. Luego aparece un Menu para cambiar los detalles actuales</li>
+                  <li>Se aplica el cambio</li>
+              </ol>
+              </li>
+              <li><button>Eliminar</button>
+              <ol>
+                  <li>Se busca el producto</li>
+                  <li>La pagina arroja un mensaje de confirmación para eliminar</li>
+                  <li>El producto es eliminado</li>
+              </ol>
+              </li>
+              <p><b>Consultar acerca de cómo hacer herramienta de consulta, CRUD, etc. Con PHP y SQL</b></p>
           </ul>
       </Fragment>
     );
