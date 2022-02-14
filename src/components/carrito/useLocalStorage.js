3c0ff31f0545f03de87a3cb14961c535fad2e5de
josetor101 <josetordecilla800@gmail.com>
2022-02-14 11:28:06 -0500

    S

--- src/components/carrito/useLocalStorage.js
+++ src/components/carrito/useLocalStorage.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export function useLocalStorage(key, initialValue) {
+export function UseLocalStorage(key, initialValue) {
     const [storedValue, setStoredValue] = useState(() => {
         try {
             const item = window.localStorage.getItem(key)
